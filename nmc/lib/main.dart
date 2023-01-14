0ff9f135bce2dd648cb3723c6ddef7e5470bba77
hg-ian-choi <hg.ian.choi@gmail.com>
2023-01-14 22:41:45 +0900

    nmc: try "BuildContext"

--- nmc/lib/main.dart
+++ nmc/lib/main.dart
@@ -12,34 +12,42 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  List<int> numbers = [];
-
-  void onClicked() {
-    setState(() {
-      numbers.add(numbers.length);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Click Count',
-              style: TextStyle(fontSize: 30),
-            ),
-            for (var n in numbers) Text('$n'),
-            IconButton(
-              onPressed: onClicked,
-              icon: const Icon(Icons.add_box_rounded),
-            )
+          children: const [
+            Title(),
           ],
         )),
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge!.color,
       ),
     );
   }
