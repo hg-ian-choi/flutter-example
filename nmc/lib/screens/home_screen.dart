e580a3d479ceb0258ac37ff176f41b10d7dd63ed
hg-ian-choi <hg.ian.choi@gmail.com>
2023-01-22 00:26:15 +0900

    nmc: try "FutureBuilder"

--- nmc/lib/screens/home_screen.dart
+++ nmc/lib/screens/home_screen.dart
@@ -2,28 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:nmc/models/webtoon_model.dart';
 import 'package:nmc/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebtoons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebtoons();
-  }
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +22,15 @@ class _HomeScreenState extends State<HomeScreen> {
             fontWeight: FontWeight.w500,
           ),
         ),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Text('Done!');
+          }
+          return const Text('Fetching data...');
+        },
       ),
     );
   }
