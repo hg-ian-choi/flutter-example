a5c16885eb70c333c66696eaa0e3785268dffe31
hg-ian-choi <hg.ian.choi@gmail.com>
2023-01-24 00:00:32 +0900

    nmc: try "GestureDetector"

--- nmc/lib/screens/home_screen.dart
+++ nmc/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nmc/models/webtoon_model.dart';
 import 'package:nmc/services/api_service.dart';
+import 'package:nmc/widgets/webtoon_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -54,33 +55,10 @@ class HomeScreen extends StatelessWidget {
       itemCount: snapshot.data!.length,
       itemBuilder: (context, index) {
         var webtoon = snapshot.data![index];
-        return Column(
-          children: [
-            Container(
-              width: 250,
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 15,
-                    offset: const Offset(10, 10),
-                    color: Colors.black.withOpacity(0.5),
-                  ),
-                ],
-              ),
-              child: Image.network(webtoon.thumbnail),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              webtoon.title,
-              style: const TextStyle(
-                fontSize: 22,
-              ),
-            ),
-          ],
+        return Webtoon(
+          id: webtoon.id,
+          title: webtoon.title,
+          thumbnail: webtoon.thumbnail,
         );
       },
       separatorBuilder: (context, index) => const SizedBox(
