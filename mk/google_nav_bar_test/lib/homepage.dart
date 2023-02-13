d41a5ebce7eab92a887d490928f31ddd90b7fd84
hg-ian-choi <hg.ian.choi@gmail.com>
2023-02-14 00:13:46 +0900

    mk-gnav: remove unnecessary codes

--- mk/google_nav_bar_test/lib/homepage.dart
+++ mk/google_nav_bar_test/lib/homepage.dart
@@ -1,6 +1,3 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
