59949aab9769d7dd363a852243d4274b32591257
hg-ian-choi <hg.ian.choi@gmail.com>
2023-02-15 22:29:42 +0900

    mk: try "slide_to_act"

---
+++ mk/slide_to_act_test/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:slide_to_act_test/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
