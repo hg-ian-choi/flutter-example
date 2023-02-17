c4b8eb5769ade18fdd12a6180217ba6a0d725281
hg-ian-choi <hg.ian.choi@gmail.com>
2023-02-18 01:05:12 +0900

    mk: modify project "confetti_test"

--- mk/confetti_test/lib/home_page.dart
+++ mk/confetti_test/lib/home_page.dart
@@ -39,7 +39,7 @@ class _HomePageState extends State<HomePage> {
         ConfettiWidget(
           confettiController: _confettiController,
           blastDirection: pi / 2,
-          colors: [
+          colors: const [
             Colors.deepPurple,
             Colors.yellow,
           ],
