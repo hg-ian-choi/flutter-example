2582792c4c9a489ea50b6af0948a95c7824d9561
hg-ian-choi <hg.ian.choi@gmail.com>
2023-02-19 23:13:39 +0900

    mk: modify project "slide_to_act_test"

--- mk/slide_to_act_test/lib/main.dart
+++ mk/slide_to_act_test/lib/main.dart
@@ -6,11 +6,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
