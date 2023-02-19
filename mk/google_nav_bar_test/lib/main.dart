31232b50cb8553dea85ed40c511caa4a47d4feee
hg-ian-choi <hg.ian.choi@gmail.com>
2023-02-19 22:51:50 +0900

    mk: modify project "google_nav_bar_test"

--- mk/google_nav_bar_test/lib/main.dart
+++ mk/google_nav_bar_test/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar_test/homepage.dart';
+import 'package:google_nav_bar_test/home_page.dart';
 
 void main() {
- runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
