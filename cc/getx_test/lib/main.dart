24288faaf59d2c4792d59b3ec6422a082fa3fbdf
hg-ian-choi <hg.ian.choi@gmail.com>
2023-02-20 23:53:53 +0900

    cc-getx: try "GetX - Reactive State Manager"

--- cc/getx_test/lib/main.dart
+++ cc/getx_test/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:getx_test/controller.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,30 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('GetX'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              GetBuilder<Controller>(
-                init: Controller(),
-                builder: (Controller _) => Text(
-                  'Current value is: ${_.x}',
-                  style: const TextStyle(fontSize: 20, color: Colors.red),
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Get.find<Controller>().increment();
-                },
-                child: const Text('Add number'),
-              ),
-            ],
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Text('Hello, World!'),
           ),
         ),
       ),
--- cc/getx_test/test/widget_test.dart
+++ cc/getx_test/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:getx_test/main.dart';
+import 'package:getx_test/simple_state_manager/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
