d00dc0b7360fe2cb809c25810e2b35f78eb50cea
hg-ian-choi <hg.ian.choi@gmail.com>
2023-02-23 23:37:51 +0900

    try "Cloud Firestore"

---
+++ firebase_test/lib/screens/message_list_screen.dart
@@ -0,0 +1,52 @@
+import 'package:firebase_test/models/message_model.dart';
+import 'package:firebase_test/services/message_service.dart';
+import 'package:flutter/material.dart';
+
+class MessageListScreen extends StatefulWidget {
+  const MessageListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MessageListScreen> createState() => _MessageListScreenState();
+}
+
+class _MessageListScreenState extends State<MessageListScreen> {
+  MessageService messageService = MessageService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Message title'),
+      ),
+      body: StreamBuilder<List<MessageModel>?>(
+        stream: messageService.streamMessages(),
+        builder: (BuildContext context, AsyncSnapshot<List<MessageModel>?> asyncSnapshot) {
+          if (!asyncSnapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (asyncSnapshot.hasError) {
+            return const Center(
+              child: Text('Error!'),
+            );
+          } else {
+            List<MessageModel> messages = asyncSnapshot.data!;
+            return Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: messages.length,
+                    itemBuilder: (BuildContext context_, int index_) => ListTile(
+                      title: Text(messages[index_].content),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ firebase_test/lib/services/message_service.dart
@@ -0,0 +1,24 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_test/models/message_model.dart';
+
+class MessageService {
+  Stream<List<MessageModel>?> streamMessages() {
+    try {
+      final Stream<QuerySnapshot> snapshots =
+          FirebaseFirestore.instance.collection('chat/K7g74GpzydHut7lUANsw/message').snapshots();
+
+      return snapshots.map((QuerySnapshot querySnapshot_) {
+        List<MessageModel> messages = <MessageModel>[];
+        for (var element_ in querySnapshot_.docs) {
+          messages.add(
+              MessageModel.fromMap(id: element_.id, map: element_.data() as Map<String, dynamic>));
+        }
+      });
+    } catch (error_) {
+      log('Error ============> ', error: error_.toString(), stackTrace: StackTrace.current);
+      return Stream.error(error_.toString());
+    }
+  }
+}
