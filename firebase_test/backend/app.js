7e3f556dd280a9383f8d5add0507fb5f8802cd6b
hg-ian-choi <hg.ian.choi@gmail.com>
2023-02-25 01:55:29 +0900

    backend add "body-parser", "cors", "dotenv" and "mongoose"

--- firebase_test/backend/app.js
+++ firebase_test/backend/app.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const port = 8080;
+
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URL);
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
