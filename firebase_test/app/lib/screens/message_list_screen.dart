7857cd726b1bc019eb6a22a51534bb80413c97b4
hg-ian-choi <hg.ian.choi@gmail.com>
2023-03-07 23:42:03 +0900

    firebase_test: modified

--- firebase_test/app/lib/screens/message_list_screen.dart
+++ firebase_test/app/lib/screens/message_list_screen.dart
@@ -15,8 +15,6 @@ class _MessageListScreenState extends State<MessageListScreen> {
 
   final TextEditingController controller = TextEditingController();
 
-  // late final WebSocketChannel channel;
-  // final channel = WebSocketChannel.connect(Uri.parse('ws://192.168.35.117:8080'));
   final IOWebSocketChannel channel = IOWebSocketChannel.connect('ws://192.168.142.234:8080');
 
   void sendMessage() {
@@ -29,21 +27,6 @@ class _MessageListScreenState extends State<MessageListScreen> {
   void initState() {
     super.initState();
     messageService.getChatList();
-    // channel = IOWebSocketChannel.connect('http://192.168.35.117:8080');
-    // , headers: {'Connection': 'upgrade', 'Upgrade': 'websocket'}
-
-    // channel.stream.listen((message) {
-    //   channel.sink.add('received!');
-    //   channel.sink.close(status.goingAway);
-    // });
-
-    // channel.stream.listen((dynamic message_) {
-    //   print('message_++++++++++++++++++++++++++++++++++++');
-    //   print(message_);
-    //   print('message_------------------------------------');
-    //   channel.sink.add('received!');
-    //   channel.sink.close(status.goingAway);
-    // });
   }
 
   @override
@@ -91,37 +74,6 @@ class _MessageListScreenState extends State<MessageListScreen> {
               },
             ),
           ),
-          // Expanded(
-          //   child: StreamBuilder<List<Chat>>(
-          //     stream: messageService.getChatList(),
-          //     builder: (BuildContext context_, AsyncSnapshot<List<Chat>> asyncSnapshot_) {
-          //       if (!asyncSnapshot_.hasData) {
-          //         return const Center(
-          //           child: CircularProgressIndicator(),
-          //         );
-          //       } else if (asyncSnapshot_.hasError) {
-          //         return const Center(
-          //           child: Text('Error!'),
-          //         );
-          //       } else {
-          //         List<MessageModel> messages = asyncSnapshot_.data!;
-          //         return Column(
-          //           mainAxisSize: MainAxisSize.max,
-          //           children: [
-          //             Expanded(
-          //               child: ListView.builder(
-          //                 itemCount: messages.length,
-          //                 itemBuilder: (BuildContext itemBuilderContext_, int index_) => ListTile(
-          //                   title: Text(messages[index_].content),
-          //                 ),
-          //               ),
-          //             ),
-          //           ],
-          //         );
-          //       }
-          //     },
-          //   ),
-          // ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
